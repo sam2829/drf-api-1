9d4e022936ec222df5384045de776bb5d9bf779d
sam2829 scarisbrick <samscarisbrick@outlook.com>
2024-02-04 14:13:31 +0000

    Added LikeList and LikeDetail generic views

--- comments/views.py
+++ comments/views.py
@@ -5,6 +5,9 @@ from .serializers import CommentSerializer, CommentDetailSerializer
 
 
 class CommentList(generics.ListCreateAPIView):
+    """
+    List comments or create a comment if logged in.
+    """
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
@@ -13,6 +16,9 @@ class CommentList(generics.ListCreateAPIView):
         serializer.save(owner=self.request.user)
 
 class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a comment, or update or delete it by id if you own it.
+    """
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = CommentDetailSerializer
     queryset = Comment.objects.all()
